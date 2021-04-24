9c9cb1c0baefc74e3ee5feb098ff996a2db750d5
GHOST <ghostdevbusiness@gmail.com>
2021-04-25 00:02:59 +0100

    feat: custom client

---
+++ src/Client.js
@@ -0,0 +1,147 @@
+import { Client as DiscordClient, Collection, MessageEmbed } from 'discord.js';
+import readdirRecursive from './util/readdirRecursive.js';
+import { basename, normalize, resolve } from 'path';
+
+export class Client extends DiscordClient {
+    #listening = false;
+    #options;
+
+    constructor(opt = {}) {
+        super(opt);
+        this.#options = opt;
+    }
+
+    generateEmbed(data) {
+        data = Object.assign(this.#options.embeds || {}, data);
+        return new MessageEmbed()
+            .setColor(data.colour)
+            .setDescription(data.description);
+    }
+
+    async loadEvents(dir, props = {}) {
+        try {
+            const events = readdirRecursive(resolve(dir))
+                .filter(
+                    (f) => f.endsWith('.js') && !basename(f).startsWith('_'),
+                )
+                .map((f) => ({ name: basename(f, '.js'), path: normalize(f) }));
+
+            for (const { name, path } of events) {
+                const { run } = await import(
+                    'file://' + path.replace('C:/', '')
+                );
+
+                if (!run) console.log(`Unable to load ${name}`);
+
+                this.on(name, run.bind(null, { client: this, ...props }));
+            }
+        } catch (e) {
+            console.error(
+                new Error(
+                    'There has been an error loading events: ' + e.message,
+                ),
+            );
+        }
+    }
+
+    async loadCommands(dir, props = []) {
+        this.commands = new Collection();
+
+        try {
+            const files = readdirRecursive(resolve(dir))
+                .filter(
+                    (f) => f.endsWith('.js') && !basename(f).startsWith('_'),
+                )
+                .map((f) => normalize(f));
+
+            const commands = [];
+
+            for (const path of files) {
+                const require = await import(
+                    'file://' + path.replace('C:/', '')
+                );
+
+                commands.push({
+                    path,
+                    meta: require.meta,
+                    props,
+                    require,
+                });
+            }
+
+            await Promise.all(commands);
+
+            for (const { meta, path, require } of commands) {
+                if (!meta)
+                    return console.log(
+                        `Unable to load a command; ID: ${meta.id}. No meta given`,
+                    );
+
+                if (!meta.commands || !Array.isArray(meta.commands))
+                    return console.log(
+                        `Unable to load a command; ID: ${meta.id}. No valid command array given`,
+                    );
+
+                for (const command of meta.commands) {
+                    if (this.commands.get(command))
+                        return console.log(
+                            `Unable to load command; ID: ${meta.id}. Command word ${command} is taken`,
+                        );
+
+                    this.commands.set(command, require);
+
+                    console.log(
+                        `Loaded command word ${command} for ${meta.id}`,
+                    );
+                }
+            }
+        } catch (e) {
+            console.error(
+                new Error(
+                    'There has been an error loading events: ' + e.message,
+                ),
+            );
+        }
+    }
+
+    start(token) {
+        this.listenForCommands();
+        this.login(token);
+    }
+
+    listenForCommands() {
+        if (this.#listening) return console.log('Unable to listen twice');
+
+        this.#listening = true;
+        this.prefix = process.env.PREFIX;
+
+        this.on('message', async (message) => {
+            console.log('asd');
+            if (message.partial) message = await message.fetch();
+            if (message.author.bot || !message.content.startsWith(this.prefix))
+                return;
+
+            const command = message.content
+                .slice(this.prefix.length)
+                .match(/^[\w\d]+/gi)[0];
+
+            const args = message.content.slice(command.length).split(' ');
+
+            const foundCommand = this.commands.get(command);
+            if (foundCommand) {
+                const result = await foundCommand.run({
+                    client: this,
+                    args,
+                    ...foundCommand.props,
+                });
+
+                if (result)
+                    message.channel.send(
+                        typeof result == 'object'
+                            ? result
+                            : this.generateEmbed({ description: result }),
+                    );
+            }
+        });
+    }
+}

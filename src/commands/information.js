7cf867ff38ed4f428279bf04cecf9303db01ba4f
GHOST <ghostdevbusiness@gmail.com>
2021-04-28 14:16:53 +0100

    fix: version

--- src/commands/information.js
+++ src/commands/information.js
@@ -1,5 +1,5 @@
 import { run as emojis } from './emoji.js';
-import package from '../../package.json';
+import { getVersion } from '../util/version.js';
 
 export const meta = {
     id: 'information',
@@ -37,7 +37,7 @@ export const run = async ({ message }) => {
             },
             {
                 name: 'Bot',
-                value: `Version: ${package.version}`,
+                value: `Version: ${getVersion()}`,
             },
         ],
         thumbnail: {

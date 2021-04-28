96600e1da2a2c4b967c917510753b82ef0997459
GHOST <ghostdevbusiness@gmail.com>
2021-04-28 14:16:41 +0100

    feat: credits command

---
+++ src/commands/credits.js
@@ -0,0 +1,19 @@
+import { getVersion } from '../util/version.js';
+
+export const meta = {
+    id: 'credits',
+    commands: ['credits'],
+    description: 'See the people that made this server what it is',
+};
+
+export const run = async ({ client }) => ({
+    title: 'Credits',
+    description: [
+        'Created and founded by <@282839711834177537>',
+        'All systems coded by <@282839711834177537>',
+        'Logo art made by <@787002534145294336>',
+        'Shop & Tag system inspired by [SourceBot](https://github.com/TheSourceCodeLLC/Source)',
+        '',
+        `Bot version: ${getVersion()}`,
+    ].join('\n'),
+});

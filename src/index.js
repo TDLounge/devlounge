bf0b4ab8fbbc633ed3a618c15257b0cfe252bd5b
GHOST <ghostdevbusiness@gmail.com>
2021-05-09 23:12:53 +0100

    feat: database system

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv/config';
 import { Client } from './Client.js';
+import { MyKV } from 'mykv';
 
 const client = new Client({
     embeds: {
@@ -7,7 +8,32 @@ const client = new Client({
     },
 });
 
-const props = {};
+const dbInformation = {
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+};
+
+const databases = new Map(
+    Object.entries({
+        xp: new MyKV({
+            ...dbInformation,
+            table: 'xp',
+        }),
+    }),
+);
+
+databases.forEach((x) => x.connect());
+
+const props = {
+    databases,
+    getDatabase: (name) => {
+        const db = databases.get(name);
+        db.connect();
+        return db;
+    },
+};
 
 client.loadEvents('src/events', props);
 client.loadCommands('src/commands', props);

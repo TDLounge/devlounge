73094659b08bb157a2edd4448968be6ab3969aa8
GHOST <ghostdevbusiness@gmail.com>
2021-05-09 23:26:03 +0100

    feat: xp command shows level

--- src/commands/xp.js
+++ src/commands/xp.js
@@ -1,17 +1,18 @@
-import { getXP } from '../util/xp.js';
+import { getXP, getLevel } from '../util/xp.js';
 
 export const meta = {
-    id: 'xp',
-    commands: ['xp'],
-    description: 'View your xp amount',
+    id: 'level',
+    commands: ['xp', 'level'],
+    description: 'View your level and xp',
 };
 
 export const run = async ({ message, getDatabase }) => {
     const db = getDatabase('xp');
 
     const xp = await getXP(db, message.author.id);
+    const level = getLevel(xp);
 
     return {
-        description: `<@${message.author.id}> You have ${xp} xp`,
+        description: `<@${message.author.id}> You are level ${level}, with ${xp} xp!`,
     };
 };

02baa061c6b2f564aea416bfe25386fd26e3142a
GHOST <ghostdevbusiness@gmail.com>
2021-08-09 23:45:51 +0100

    chore: remove old code

--- src/util/version.js
+++
@@ -1,7 +0,0 @@
-import { readFileSync } from 'fs';
-import { join } from 'path';
-
-export function getVersion() {
-    const pkg = readFileSync(join(process.cwd(), 'package.json'), 'utf-8');
-    return JSON.parse(pkg).version;
-}

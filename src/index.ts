473adf6d0d02c289a3c606299c1b0789f9e5dce8
GHOST <ghostdevbusiness@gmail.com>
2021-08-09 23:53:17 +0100

    feat: jellycommand config

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import 'dotenv/config';
+import { Client, Intents } from 'discord.js';
+import { JellyCommands } from 'jellycommands';
+
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+const jelly = new JellyCommands(client, {
+    prefix: '.',
+
+    messages: {
+        unknownCommand: {
+            embeds: [{ description: 'Unknown Command', color: '#cf4a4a' }],
+        },
+    },
+});
+
+jelly.slashCommands.load('dev/slashCommands');
+
+client.login(process.env.TOKEN);
+
+client.on('ready', () => {
+    console.log('Online');
+    jelly.slashCommands.register();
+});

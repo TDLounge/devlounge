e82b7df329e665fa9be5c9cd1c4fc0d2a59c7578
GHOST <ghostdevbusiness@gmail.com>
2021-12-12 00:11:11 +0000

    feat: coins command

---
+++ src/commands/coins.ts
@@ -0,0 +1,44 @@
+import { command } from 'jellycommands';
+import { Knex } from 'knex';
+
+export default command({
+    name: 'coins',
+    description: 'Check yours or others coins',
+
+    options: [
+        {
+            name: 'user',
+            description: 'The user whos coins you want to check',
+            type: '6',
+            required: false,
+        },
+    ],
+
+    global: true,
+    dev: true,
+
+    defer: true,
+
+    run: async ({ interaction, client }) => {
+        const userId =
+            interaction.options.getUser('user')?.id ?? interaction.user.id;
+        const db = client.props.get<Knex>('db');
+
+        const user = await db('user')
+            .select('coins')
+            .where({ id: userId })
+            .first();
+
+        // prettier-ignore
+        const description = `<@${userId}> ${userId == interaction.user.id ? 'you have' : 'has'} ${user?.coins || 0} coins`;
+
+        interaction.followUp({
+            embeds: [
+                {
+                    color: '#cf4a4a',
+                    description,
+                },
+            ],
+        });
+    },
+});

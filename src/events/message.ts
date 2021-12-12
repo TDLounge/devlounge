fe1a518d08a148ee130623d3802cf4d1cf70436c
GHOST <ghostdevbusiness@gmail.com>
2021-12-12 00:01:06 +0000

    feat: coins and xp

---
+++ src/events/message.ts
@@ -0,0 +1,37 @@
+import { event } from 'jellycommands';
+import { Knex } from 'knex';
+
+const ran = (min: number, max: number) =>
+    Math.floor(Math.random() * (max - min + 1) + min);
+
+const cooldown = new Map();
+
+export default event({
+    name: 'messageCreate',
+
+    run: async ({ client }, message) => {
+        console.log(cooldown);
+        // prettier-ignore
+        if (cooldown.has(message.author.id) && cooldown.get(message.author.id) > Date.now()) return;
+
+        const db = client.props.get<Knex>('db');
+
+        const user = await db('user')
+            .select('coins', 'xp')
+            .where({ id: message.author.id })
+            .first();
+
+        const coins = ran(4, 18);
+        const xp = ran(6, 32);
+
+        if (user) {
+            await db('user')
+                .update({ coins: user.coins + coins, xp: user.xp + xp })
+                .where({ id: message.author.id });
+        } else {
+            await db('user').insert({ id: message.author.id, coins, xp });
+        }
+
+        cooldown.set(message.author.id, Date.now() + ran(18000, 32000));
+    },
+});

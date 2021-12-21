b8f08d690332da16c6560e4492d35525ac0e7c53
GHOST <ghostdevbusiness@gmail.com>
2021-12-21 00:36:04 +0000

    feat: tags

---
+++ src/commands/tags.ts
@@ -0,0 +1,42 @@
+import { ApplicationCommandOptionTypes } from 'discord.js/typings/enums';
+import { command } from 'jellycommands';
+import { Knex } from 'knex';
+
+export default command({
+    name: 'tags',
+    description: 'Tags are pretty cool',
+
+    options: [
+        {
+            name: 'tag',
+            description: 'the tag',
+            type: ApplicationCommandOptionTypes.STRING,
+            required: true,
+        },
+    ],
+
+    global: true,
+    defer: true,
+    dev: true,
+
+    run: async ({ interaction, client }): Promise<void> => {
+        const name = interaction.options.getString('tag', true);
+        const db = client.props.get<Knex>('db');
+
+        const tag = await db('tag').select('content').where({ name }).first();
+
+        if (!tag)
+            return void interaction.followUp({
+                embeds: [
+                    {
+                        color: '#cf4a4a',
+                        description: `Unable to find tag: \`${name}\``,
+                    },
+                ],
+            });
+
+        interaction.followUp({
+            embeds: [{ color: '#cf4a4a', description: tag.content }],
+        });
+    },
+});

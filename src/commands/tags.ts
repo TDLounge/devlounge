f847e5722cc1065cfa8c16288d2c5c82fa4a41a3
GHOSt <ghostdevbusiness@gmail.com>
2022-01-10 19:53:37 +0000

    feat: tags update

--- src/commands/tags.ts
+++ src/commands/tags.ts
@@ -8,37 +8,87 @@ export default command({
 
     options: [
         {
-            name: 'tag',
-            description: 'the tag',
-            type: ApplicationCommandOptionTypes.STRING,
-            required: true,
+            name: 'view',
+            description: 'View all the tags',
+            type: ApplicationCommandOptionTypes['SUB_COMMAND'],
+        },
+        {
+            name: 'get',
+            description: 'Get a tag by name',
+            type: ApplicationCommandOptionTypes['SUB_COMMAND'],
+            options: [
+                {
+                    name: 'tag',
+                    description: 'the tag',
+                    type: ApplicationCommandOptionTypes.STRING,
+                    required: true,
+                },
+            ],
         },
     ],
 
     global: true,
     defer: true,
 
+    dev: true,
+
     run: async ({ interaction, client }): Promise<void> => {
-        const name = interaction.options.getString('tag', true);
+        const subCommand = interaction.options.getSubcommand(true);
         const db = client.props.get<Knex>('db');
 
-        const tag = await db('tag')
-            .select<{ content: string }>('content')
-            .where({ name })
-            .first();
+        if (subCommand == 'get') {
+            const name = interaction.options.getString('tag', true);
 
-        if (!tag)
+            const tag = await db('tag')
+                .select<{ content: string }>('content')
+                .where({ name })
+                .first();
+
+            if (!tag)
+                return void interaction.followUp({
+                    embeds: [
+                        {
+                            color: '#cf4a4a',
+                            description: `Unable to find tag: \`${name}\``,
+                        },
+                    ],
+                });
+
             return void interaction.followUp({
                 embeds: [
                     {
                         color: '#cf4a4a',
-                        description: `Unable to find tag: \`${name}\``,
+                        description: tag.content,
+                        footer: {
+                            text: `/tag ${name}`,
+                        },
                     },
                 ],
             });
+        }
+
+        const tags = await db('tag').select<
+            { name: string; category: string }[]
+        >('name', 'category');
+
+        const fields: Record<string, string[]> = {};
+
+        for (const { name, category } of tags) {
+            const existing = fields[category] || [];
+            fields[category] = [...existing, name];
+        }
 
         interaction.followUp({
-            embeds: [{ color: '#cf4a4a', description: tag.content }],
+            embeds: [
+                {
+                    color: '#cf4a4a',
+                    title: 'Tags',
+                    fields: Object.entries(fields).map(([name, value]) => ({
+                        name,
+                        value: value.map((x) => `\`${x}\``).join(', '),
+                    })),
+                },
+            ],
         });
     },
 });
